bc0bd435bdf1c97b8a3e2565a7bf507c4bb9f159
João Vitor <jvolima2004@gmail.com>
2023-09-26 14:26:44 -0300

    test: process transaction should return unprocessable entity when card expiration date is blank

--- src/test/java/com/jvolima/desafiopagarme/controllers/TransactionControllerIT.java
+++ src/test/java/com/jvolima/desafiopagarme/controllers/TransactionControllerIT.java
@@ -210,4 +210,19 @@ public class TransactionControllerIT {
 
         result.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
     }
+
+    @Test
+    public void processTransactionShouldReturnUnprocessableEntityWhenCardExpirationDateIsBlank() throws Exception {
+        TransactionDTO transactionDTO = Factory.createTransactionDTO();
+        transactionDTO.setCardExpirationDate(null);
+        String jsonBody = objectMapper.writeValueAsString(transactionDTO);
+
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.post("/transactions")
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+    }
 }
