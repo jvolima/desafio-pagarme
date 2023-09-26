b17a41b5e9b2160ad3d380fc7ad8e056e167e83e
João Vitor <jvolima2004@gmail.com>
2023-09-26 14:36:32 -0300

    test: process transaction should return unprocessable entity when cvv is null

--- src/test/java/com/jvolima/desafiopagarme/controllers/TransactionControllerIT.java
+++ src/test/java/com/jvolima/desafiopagarme/controllers/TransactionControllerIT.java
@@ -255,4 +255,19 @@ public class TransactionControllerIT {
 
         result.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
     }
+
+    @Test
+    public void processTransactionShouldReturnUnprocessableEntityWhenCvvIsNull() throws Exception {
+        TransactionDTO transactionDTO = Factory.createTransactionDTO();
+        transactionDTO.setCvv(null);
+        String jsonBody = objectMapper.writeValueAsString(transactionDTO);
+
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.post("/transactions")
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(MockMvcResultMatchers.status().isUnprocessableEntity());
+    }
 }
